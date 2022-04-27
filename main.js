b3b2f8ea3fd824b9afcfbf05ac054d472f47cb3d
gemy26 <102263771+gemy26@users.noreply.github.com>
2022-04-27 05:54:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let card=document.querySelector(".container");
+
+function showdata([img,product,category,brand,rate,price,offer]){
+let code=`
+<div class="card">
+         <img src="${img}" alt="${product}">
+         <div class="text">
+             <p class="category">${category}</p>
+             <p class="name">${brand}</p>
+             <h5 class="rate">${rate}</h5>
+             <p class="price">${price}</p>
+             <p class="sale">${offer}</p>
+         </div>
+     </div>
+`;
+card.innerHTML+=code;
+}
+let item1 = ["k552.jpg", "Keyboard", "K552 rgp blue switch ///........../.......................................", "Redragon", "★★★★★","30$","10% off"];
+for(let i=0;i<20;i++)
+{
+    showdata(item1);
+}
